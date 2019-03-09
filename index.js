46d5c7411f6bd98c1d1b29fc83d61fce8d2be6fa
Max Musing <max.musing@gmail.com>
2019-03-09 13:23:15 -0500

    Add various daily activities

--- index.js
+++ index.js
@@ -9,22 +9,32 @@ async function run() {
 	const password = process.env.PASSWORD;
 
 	if (!browser || !username || !password) {
-		console.error('Error reading environment variables. Ensure you have created a .env file with the variables: BROWSER, USERNAME, and PASSWORD.')
+		console.error(
+			'Error reading environment variables. Ensure you have created a .env file with the variables: BROWSER, USERNAME, and PASSWORD.'
+		);
 		return;
 	}
 
 	const driver = await new Builder().forBrowser(browser).build();
 
 	try {
-		await driver.manage().window().maximize();
+		await driver
+			.manage()
+			.window()
+			.maximize();
 		await driver.get('http://www.neopets.com');
 
 		await login(driver, username, password);
 		await collectInterest(driver);
-		await buyStocks(driver, 1000, 20);
+		await buyStocks(driver, 1000, 17);
 		await sellStocks(driver, 30);
-	} catch(e) {
-		console.error('Error while running.\n', e);
+		await shopOfOffers(driver);
+		await anchorManagement(driver);
+		await tdmbgpop(driver);
+		await fruitMachine(driver);
+		await coltzansShrine(driver);
+	} catch (error) {
+		console.error('Error while running.\n', error);
 	} finally {
 		console.log('Bot completed.');
 		await driver.sleep(3000);
@@ -36,14 +46,17 @@ async function login(driver, username, password) {
 	const xpath = {
 		loginLink: '//*[@id="header"]/table/tbody/tr[1]/td[3]/a[1]',
 		usernameField: '//*[@id="templateLoginPopupUsername"]',
-		passwordField: '//*[@id="templateLoginPopup"]/div/form/table/tbody/tr[2]/td[2]/input',
+		passwordField:
+			'//*[@id="templateLoginPopup"]/div/form/table/tbody/tr[2]/td[2]/input',
 		logoutLink: '//*[@id="logout_link"]',
-	}
+	};
 
 	await driver.navigate().to('http://www.neopets.com');
 	await driver.findElement(By.xpath(xpath.loginLink)).click();
 	await driver.findElement(By.xpath(xpath.usernameField)).sendKeys(username);
-	await driver.findElement(By.xpath(xpath.passwordField)).sendKeys(password, Key.RETURN);
+	await driver
+		.findElement(By.xpath(xpath.passwordField))
+		.sendKeys(password, Key.RETURN);
 	await driver.wait(until.elementLocated(By.xpath(xpath.logoutLink)));
 
 	console.log('Login completed.');
@@ -51,9 +64,11 @@ async function login(driver, username, password) {
 
 async function collectInterest(driver) {
 	const xpath = {
-		interestBox: '//*[@id="content"]/table/tbody/tr/td[2]/table[2]',
-		button: '//*[@id="content"]/table/tbody/tr/td[2]/table[2]/tbody/tr/td/div/table/tbody/tr[2]/td/div/form/input[2]',
-	}
+		interestBox:
+			'//*[@id="content"]/table/tbody/tr/td[2]/table[2]/tbody/tr/td/div/table/tbody/tr[2]/td',
+		button:
+			'//*[@id="content"]/table/tbody/tr/td[2]/table[2]/tbody/tr/td/div/table/tbody/tr[2]/td/div/form/input[2]',
+	};
 
 	await driver.navigate().to('http://www.neopets.com/bank.phtml');
 	await driver.wait(until.elementLocated(By.xpath(xpath.interestBox)));
@@ -64,7 +79,7 @@ async function collectInterest(driver) {
 		console.log('Interest collection completed.');
 	} catch (e) {
 		if (e.name === 'NoSuchElementError') {
-			console.log('Interest already collected.')
+			console.log('Interest already collected.');
 		} else {
 			console.error('Error collecting bank interest.', e);
 		}
@@ -75,15 +90,23 @@ async function buyStocks(driver, numShares, buyPrice) {
 	const xpath = {
 		stockField: '//*[@id="content"]/table/tbody/tr/td[2]/div[2]/form/input[2]',
 		stockTable: '//*[@id="content"]/table/tbody/tr/td[2]/div[2]/table/tbody',
-		stockLink: '//*[@id="content"]/table/tbody/tr/td[2]/div[2]/table/tbody/tr[1]/td[2]/a/b/font',
-		stockNumberField: '//*[@id="content"]/table/tbody/tr/td[2]/div[2]/form/table/tbody/tr[2]/td[2]/input',
-	}
+		stockLink:
+			'//*[@id="content"]/table/tbody/tr/td[2]/div[2]/table/tbody/tr[1]/td[2]/a/b/font',
+		stockNumberField:
+			'//*[@id="content"]/table/tbody/tr/td[2]/div[2]/form/table/tbody/tr[2]/td[2]/input',
+	};
 
-	await driver.navigate().to('http://www.neopets.com/stockmarket.phtml?type=list');
-	let stockField = await driver.wait(until.elementLocated(By.xpath(xpath.stockField)));
+	await driver
+		.navigate()
+		.to('http://www.neopets.com/stockmarket.phtml?type=list');
+	let stockField = await driver.wait(
+		until.elementLocated(By.xpath(xpath.stockField))
+	);
 	await stockField.sendKeys(Key.SPACE, Key.RETURN);
 
-	let stockTable = await driver.wait(until.elementLocated(By.xpath(xpath.stockTable)));
+	let stockTable = await driver.wait(
+		until.elementLocated(By.xpath(xpath.stockTable))
+	);
 	let stocks = await stockTable.findElements(By.css('tr:not(:first-child)'));
 
 	let stockInfo = [];
@@ -91,7 +114,9 @@ async function buyStocks(driver, numShares, buyPrice) {
 	for (stock of stocks) {
 		try {
 			let stockLink = await stock.findElement(By.css('td:nth-child(2) a b'));
-			let stockPrice = await stock.findElement(By.css('td:nth-child(6)')).getText();
+			let stockPrice = await stock
+				.findElement(By.css('td:nth-child(6)'))
+				.getText();
 
 			stockInfo.push({
 				link: stockLink,
@@ -106,16 +131,22 @@ async function buyStocks(driver, numShares, buyPrice) {
 		}
 	}
 
-	stockInfo = stockInfo.filter((stock) => stock.price >= 15 && stock.price <= buyPrice);
+	stockInfo = stockInfo.filter(
+		stock => stock.price >= 15 && stock.price <= buyPrice
+	);
 	stockInfo = stockInfo.sort((a, b) => a.price - b.price);
 
 	if (stockInfo.length > 0) {
 		await stockInfo[0].link.click();
 
-		let stockLink = await driver.wait(until.elementLocated(By.xpath(xpath.stockLink)));
+		let stockLink = await driver.wait(
+			until.elementLocated(By.xpath(xpath.stockLink))
+		);
 		await stockLink.click();
 
-		let stockNumberField = await driver.wait(until.elementLocated(By.xpath(xpath.stockNumberField)));
+		let stockNumberField = await driver.wait(
+			until.elementLocated(By.xpath(xpath.stockNumberField))
+		);
 		await stockNumberField.sendKeys(numShares.toString(), Key.RETURN);
 	}
 
@@ -127,19 +158,31 @@ async function sellStocks(driver, sellPrice) {
 	const xpath = {
 		stockTable: '//*[@id="postForm"]/table[1]/tbody',
 		sellButton: '//*[@id="show_sell"]/center/input',
-	}
+	};
 
-	await driver.navigate().to('http://www.neopets.com/stockmarket.phtml?type=portfolio');
-	let stockTable = await driver.wait(until.elementLocated(By.xpath(xpath.stockTable)));
-	let stocks = await stockTable.findElements(By.css('tr:not(:nth-child(-n+2)):not(:last-child):nth-child(odd)'));
+	await driver
+		.navigate()
+		.to('http://www.neopets.com/stockmarket.phtml?type=portfolio');
+	let stockTable = await driver.wait(
+		until.elementLocated(By.xpath(xpath.stockTable))
+	);
+	let stocks = await stockTable.findElements(
+		By.css('tr:not(:nth-child(-n+2)):not(:last-child):nth-child(odd)')
+	);
 
 	let stockInfo = [];
 
 	for (stock of stocks) {
 		try {
-			let stockArrow = await stock.findElement(By.css('td:first-child img:first-child'));
-			let stockShareTable = await stock.findElement(By.xpath('following-sibling::*[1]'));
-			let stockPrice = await stock.findElement(By.css('td:nth-child(4)')).getText();
+			let stockArrow = await stock.findElement(
+				By.css('td:first-child img:first-child')
+			);
+			let stockShareTable = await stock.findElement(
+				By.xpath('following-sibling::*[1]')
+			);
+			let stockPrice = await stock
+				.findElement(By.css('td:nth-child(4)'))
+				.getText();
 
 			stockInfo.push({
 				arrow: stockArrow,
@@ -155,17 +198,21 @@ async function sellStocks(driver, sellPrice) {
 		}
 	}
 
-	stockInfo = stockInfo.filter((stock) => stock.price >= sellPrice);
+	stockInfo = stockInfo.filter(stock => stock.price >= sellPrice);
 	stockInfo = stockInfo.sort((a, b) => a.price - b.price);
 
 	if (stockInfo.length > 0) {
 		for (stock of stockInfo) {
 			await stock.arrow.click();
 
-			let shares = await stock.shareTable.findElements(By.css('tbody tr:not(:first-child)'));
+			let shares = await stock.shareTable.findElements(
+				By.css('tbody tr:not(:first-child)')
+			);
 
 			for (share of shares) {
-				let numShares = await share.findElement(By.css('td:first-child')).getText();
+				let numShares = await share
+					.findElement(By.css('td:first-child'))
+					.getText();
 				let shareField = await share.findElement(By.css('input'));
 				await shareField.sendKeys(numShares);
 			}
@@ -176,4 +223,81 @@ async function sellStocks(driver, sellPrice) {
 
 	await driver.sleep(3000);
 	console.log('Selling stocks completed.');
+}
+
+async function shopOfOffers(driver) {
+	const xpath = {
+		event: '//*[@id="content"]/table/tbody/tr/td[2]/table',
+	};
+
+	await driver
+		.navigate()
+		.to('http://www.neopets.com/shop_of_offers.phtml?slorg_payout=yes');
+	await driver.wait(until.elementLocated(By.xpath(xpath.event)));
+	console.log('Shop of offers completed.');
+}
+
+async function anchorManagement(driver) {
+	const xpath = {
+		button: '//*[@id="btn-fire"]/a/div',
+		prizeImage: '//*[@id="krawk-island"]/div[3]/div/img',
+	};
+
+	await driver
+		.navigate()
+		.to('http://www.neopets.com/pirates/anchormanagement.phtml');
+	const button = await driver.wait(
+		until.elementLocated(By.xpath(xpath.button))
+	);
+	await button.click();
+	await driver.wait(until.elementLocated(By.xpath(xpath.prizeImage)));
+	console.log('Anchor management completed.');
+}
+
+async function tdmbgpop(driver) {
+	const xpath = {
+		button: '//*[@id="content"]/table/tbody/tr/td[2]/div[3]/form/input[2]',
+	};
+
+	await driver
+		.navigate()
+		.to('http://www.neopets.com/faerieland/tdmbgpop.phtml');
+	const button = await driver.wait(
+		until.elementLocated(By.xpath(xpath.button))
+	);
+	await button.click();
+
+	await driver.sleep(3000);
+	console.log('TDMGBPOP completed.');
+}
+
+async function fruitMachine(driver) {
+	const xpath = {
+		button: '//*[@id="content"]/table/tbody/tr/td[2]/div[2]/form/input[3]',
+	};
+
+	await driver.navigate().to('http://www.neopets.com/desert/fruit/index.phtml');
+	const button = await driver.wait(
+		until.elementLocated(By.xpath(xpath.button))
+	);
+	await button.click();
+
+	await driver.sleep(10000);
+	console.log('Fruit machine completed.');
+}
+
+async function coltzansShrine(driver) {
+	const xpath = {
+		button:
+			'//*[@id="content"]/table/tbody/tr/td[2]/div[2]/div/form[1]/input[2]',
+	};
+
+	await driver.navigate().to('http://www.neopets.com/desert/shrine.phtml');
+	const button = await driver.wait(
+		until.elementLocated(By.xpath(xpath.button))
+	);
+	await button.click();
+
+	await driver.sleep(3000);
+	console.log("Coltzan's shrine completed.");
 }
